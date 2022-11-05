0d4ad6b03ccaeef43b6ac2e22d30a9fafc0651a5
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-11-05 16:51:26 -0700

    Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(READ)
+
+	@KBD
+	D=M
+	@WHITE
+	D;JEQ
+
+(BLACK)
+
+	@color  // color = black
+	M=-1
+	@FILL
+	0;JMP
+
+(WHITE)
+
+	@color	// color = white
+	M=0
+
+(FILL)
+
+	@i			// i = -1
+	MD=-1
+
+(FILL_LOOP)
+	@i			// D = i++
+	M=M+1
+	D=M
+
+	@SCREEN // D = *SCREEN + i
+	D=D+A
+
+	@ptr		// ptr = D
+	M=D
+
+	@color	// D = color
+	D=M
+
+	@ptr    // *ptr = color
+	A=M
+	M=D
+
+	@i
+	D=M
+	@8191
+	D=A-D
+	@FILL_LOOP
+	D;JGT
+
+(DONE)
+
+	@READ
+	0;JMP
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,31 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+	@R2    // R2 = 0
+	M=0
+
+	@R0    // if R0 == 0 then DONE
+	D=M
+	@DONE
+	D;JEQ
+
+	@R1    // if R1 == 0 then DONE
+	D=M
+	@DONE
+	D;JEQ
+
+(LOOP)
+
+	@R1    // R2 += R1
+	D=M
+	@R2
+	M=D+M
+
+	@R0    // if --R0 > 0 then LOOP
+	MD=M-1
+	@LOOP
+	D;JGT
+
+(DONE)
+
